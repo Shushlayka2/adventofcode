99790ca456a927d13d1b03874eb3ef8e6827b843
Bulat Latypov <shushlaykin@Bulats-MBP.home>
2023-12-02 01:21:52 +0100

    The first task is completed

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using var streamReader = new StreamReader("input.txt");
+var line = "";
+var sum = 0;
+
+while ((line = streamReader.ReadLine()) != null)
+{
+    sum += GetCalibrationValue(line);
+}
+
+Console.WriteLine(sum);
+
+public partial class Program
+{
+
+    private static int GetCalibrationValue(string inputLine)
+    {
+        var firstNum = inputLine.FirstOrDefault(ch => char.IsDigit(ch)) - '0';
+        var lastNum = inputLine.LastOrDefault(ch => char.IsDigit(ch)) - '0';
+
+        return firstNum * 10 + lastNum;
+    }
+}
